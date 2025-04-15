2ecd6a79c8e239a0b3c277072fb657e15691bb85
ibrahimeltbakh <eltabakh987@gmail.com>
2025-04-15 14:40:16 +0200

    install react query

--- src/pages/Layout.jsx
+++ src/pages/Layout.jsx
@@ -1,13 +1,15 @@
-import React from 'react'
-import NavBar from '../components/navbar/NavBar'
-import { Outlet } from 'react-router-dom'
-import Footer from '../components/footer/Footer'
+import React from "react";
+import NavBar from "../components/navbar/NavBar";
+import { Outlet } from "react-router-dom";
+import Footer from "../components/footer/Footer";
 export default function Layout() {
-    return (
-        <div>
-            <NavBar/>
-            <Outlet/>
-            <Footer/>
-        </div>
-    )
+  return (
+    <div>
+      <NavBar />
+      <div className="min-h-screen">
+        <Outlet />
+      </div>
+      <Footer />
+    </div>
+  );
 }
